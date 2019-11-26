ca41582495e2b32af1a82d24c33aa61ccd1a7889
Reuben Pasquini <reuben@frickjack.com>
2019-11-26 17:32:27 -0600

    test(runTest): basic modsecurity test

---
+++ test/runTest.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Run some tests
+#
+
+if [[ $# -lt 1 || $1 =~ ^-*h(elp)? ]]; then
+    cat - <<EOM
+  bash runTest.sh nginx-docker-image
+EOM
+fi
+
+image="$1"
+
+docker run -d --rm --name nginx-test -v "$(pwd)/conf.d:/etc/nginx/conf.d" -v "$(pwd)/modsec:/etc/nginx/modsec" -v "$(pwd)/nginx.conf:/etc/nginx.conf" -p 9080:80 -p 9085:8085 "$image"
+sleep 10
+curl -D - http://localhost:9085/
+curl -D - http://localhost:9080/
+curl -D - http://localhost:9080/foo?testparam=thisisatestofmodsecurity
