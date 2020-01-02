de6c5fbc4858a0f398116cc16eb220b83b23551a
Reuben Pasquini <reuben@frickjack.com>
2020-01-02 11:27:52 -0600

    test(nginx): with mod_http_js

---
+++ test/helpers.js
@@ -0,0 +1,8 @@
+/**
+ * returns absolute value of a number
+ */
+function MathAbs(x) {
+  x = +x;
+  return (x > 0) ? x : 0 - x;
+}
+
--- test/runTest.sh
+++ test/runTest.sh
@@ -3,17 +3,18 @@
 # Run some tests
 #
 
-if [[ $# -lt 1 || $1 =~ ^-*h(elp)? ]]; then
+if [[ $# -lt 1 || "$1" =~ ^-*h(elp)? ]]; then
     cat - <<EOM
   bash runTest.sh nginx-docker-image
-  exit 1
 EOM
+  exit 1
 fi
 
 image="$1"
 
-docker run -d --rm --name nginx-test -v "$(pwd)/conf.d:/etc/nginx/conf.d" -v "$(pwd)/modsec:/etc/nginx/modsec" -v "$(pwd)/nginx.conf:/etc/nginx/nginx.conf" -p 9080:80 -p 9085:8085 "$image"
+docker run -d --rm --name nginx-test -v "$(pwd)/conf.d:/etc/nginx/conf.d" -v "$(pwd)/modsec:/etc/nginx/modsec" -v "$(pwd)/nginx.conf:/etc/nginx/nginx.conf" -v "$(pwd)/helpers.js:/etc/nginx/helpers.js" -p 9080:80 -p 9085:8085 "$image"
 sleep 10
 curl -D - http://localhost:9085/
 curl -D - http://localhost:9080/
 curl -D - http://localhost:9080/foo?testparam=thisisatestofmodsecurity
+
