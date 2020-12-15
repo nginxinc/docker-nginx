14e73942f37a7f4c1eb886a28b1e183555b4eacd
Konstantin Pavlov <thresh@videolan.org>
2020-12-15 17:37:27 +0300

    10-liste-on-ipv6-by-default: use "info" instead of error
    By popular request in https://github.com/nginxinc/docker-nginx/issues/419

--- mainline/debian-perl/10-listen-on-ipv6-by-default.sh
+++ mainline/debian-perl/10-listen-on-ipv6-by-default.sh
@@ -8,17 +8,17 @@ DEFAULT_CONF_FILE="etc/nginx/conf.d/default.conf"
 
 # check if we have ipv6 available
 if [ ! -f "/proc/net/if_inet6" ]; then
-    echo >&3 "$ME: error: ipv6 not available"
+    echo >&3 "$ME: info: ipv6 not available"
     exit 0
 fi
 
 if [ ! -f "/$DEFAULT_CONF_FILE" ]; then
-    echo >&3 "$ME: error: /$DEFAULT_CONF_FILE is not a file or does not exist"
+    echo >&3 "$ME: info: /$DEFAULT_CONF_FILE is not a file or does not exist"
     exit 0
 fi
 
 # check if the file can be modified, e.g. not on a r/o filesystem
-touch /$DEFAULT_CONF_FILE 2>/dev/null || { echo >&3 "$ME: error: can not modify /$DEFAULT_CONF_FILE (read-only file system?)"; exit 0; }
+touch /$DEFAULT_CONF_FILE 2>/dev/null || { echo >&3 "$ME: info: can not modify /$DEFAULT_CONF_FILE (read-only file system?)"; exit 0; }
 
 # check if the file is already modified, e.g. on a container restart
 grep -q "listen  \[::]\:80;" /$DEFAULT_CONF_FILE && { echo >&3 "$ME: info: IPv6 listen already enabled"; exit 0; }
@@ -26,11 +26,11 @@ grep -q "listen  \[::]\:80;" /$DEFAULT_CONF_FILE && { echo >&3 "$ME: info: IPv6 
 if [ -f "/etc/os-release" ]; then
     . /etc/os-release
 else
-    echo >&3 "$ME: error: can not guess the operating system"
+    echo >&3 "$ME: info: can not guess the operating system"
     exit 0
 fi
 
-echo >&3 "$ME: Getting the checksum of /$DEFAULT_CONF_FILE"
+echo >&3 "$ME: info: Getting the checksum of /$DEFAULT_CONF_FILE"
 
 case "$ID" in
     "debian")
@@ -48,7 +48,7 @@ case "$ID" in
         }
         ;;
     *)
-        echo >&3 "$ME: error: Unsupported distribution"
+        echo >&3 "$ME: info: Unsupported distribution"
         exit 0
         ;;
 esac
@@ -56,6 +56,6 @@ esac
 # enable ipv6 on default.conf listen sockets
 sed -i -E 's,listen       80;,listen       80;\n    listen  [::]:80;,' /$DEFAULT_CONF_FILE
 
-echo >&3 "$ME: Enabled listen on IPv6 in /$DEFAULT_CONF_FILE"
+echo >&3 "$ME: info: Enabled listen on IPv6 in /$DEFAULT_CONF_FILE"
 
 exit 0
