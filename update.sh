fa2213a76da35137a5aee565cef5095a209aeb3d
Konstantin Pavlov <thresh@videolan.org>
2021-02-16 20:28:33 +0300

    Updated nginx to 1.19.7 and njs to 0.5.1 for mainline

--- update.sh
+++ update.sh
@@ -10,11 +10,11 @@ declare branches=(
 )
 
 declare -A nginx=(
-    [mainline]='1.19.6'
+    [mainline]='1.19.7'
     [stable]='1.18.0'
 )
 
-defaultnjs='0.5.0'
+defaultnjs='0.5.1'
 declare -A njs=(
     [stable]='0.4.4'
 )
