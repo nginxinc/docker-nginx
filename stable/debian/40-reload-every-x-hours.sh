451f81970045e40866500887ed79b3849b38e2da
Aleksejs Spiridonovs <aspiridonovs@3pc.de>
2021-02-22 23:04:26 +0200

    UX: output during reload; FIX: chmod +x

--- stable/debian/40-reload-every-x-hours.sh
+++ stable/debian/40-reload-every-x-hours.sh
@@ -8,7 +8,7 @@ ME=$(basename $0)
 
 start_background_reload() {
   echo >&3 "$ME: Reloading Nginx every $NGINX_ENTRYPOINT_NGINX_RELOAD_EVERY_X_HOURS hours"
-  while :; do sleep ${NGINX_ENTRYPOINT_NGINX_RELOAD_EVERY_X_HOURS}h; nginx -s reload; done &
+  while :; do sleep ${NGINX_ENTRYPOINT_NGINX_RELOAD_EVERY_X_HOURS}h; echo >&3 "$ME: Reloading Nginx ..." && nginx -s reload; done &
 }
 
 start_background_reload
