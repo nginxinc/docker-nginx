31197335909b5cbf64a8b6f4edbb8d20c2b1540d
Aleksejs Spiridonovs <aspiridonovs@3pc.de>
2021-02-22 22:49:03 +0200

    FEAT: entrypoint script background-reloading Nginx (think LetsEncrypt certs)

---
+++ mainline/alpine/40-reload-every-x-hours.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+ME=$(basename $0)
+
+[ "${NGINX_ENTRYPOINT_NGINX_RELOAD_EVERY_X_HOURS:-}" ] || exit 0
+
+start_background_reload() {
+  echo >&3 "$ME: Reloading Nginx every $NGINX_ENTRYPOINT_NGINX_RELOAD_EVERY_X_HOURS hours"
+  while :; do sleep ${NGINX_ENTRYPOINT_NGINX_RELOAD_EVERY_X_HOURS}h; nginx -s reload; done &
+}
+
+start_background_reload
