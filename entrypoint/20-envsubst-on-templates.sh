f5d8826cafbd40b6ac8a4b0a2daa015a25f55e8c
aosynsky <anton.osynskyi@gmail.com>
2022-03-28 16:22:51 +0300

    Update 20-envsubst-on-templates.sh
    fix issue-645 https://github.com/nginxinc/docker-nginx/issues/645

--- entrypoint/20-envsubst-on-templates.sh
+++ entrypoint/20-envsubst-on-templates.sh
@@ -13,7 +13,7 @@ auto_envsubst() {
   defined_envs=$(printf '${%s} ' $(env | cut -d= -f1))
   [ -d "$template_dir" ] || return 0
   if [ ! -w "$output_dir" ]; then
-    echo >&3 "$ME: ERROR: $template_dir exists, but $output_dir is not writable"
+    echo >&2 "$ME: ERROR: $template_dir exists, but $output_dir is not writable"
     return 0
   fi
   find "$template_dir" -follow -type f -name "*$suffix" -print | while read -r template; do
@@ -22,7 +22,7 @@ auto_envsubst() {
     subdir=$(dirname "$relative_path")
     # create a subdirectory where the template file exists
     mkdir -p "$output_dir/$subdir"
-    echo >&3 "$ME: Running envsubst on $template to $output_path"
+    echo >&2 "$ME: Running envsubst on $template to $output_path"
     envsubst "$defined_envs" < "$template" > "$output_path"
   done
 }
