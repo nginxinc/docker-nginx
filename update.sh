8c1a47c7f63a720e358028dcd17efb970b13ad5b
Jesper Noordsij <jesper@sslleiden.nl>
2024-05-29 17:09:10 +0200

    Update stable to Alpine 3.20

--- update.sh
+++ update.sh
@@ -56,7 +56,7 @@ declare -A debian=(
 
 declare -A alpine=(
     [mainline]='3.20'
-    [stable]='3.19'
+    [stable]='3.20'
 )
 
 # When we bump njs version in a stable release we don't move the tag in the
